1ef8ce211a16e0faa8389fcc883b2c418e4b99ca
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2014-01-07 15:40:34 +0000

    Patch to fix Palladio-290
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Commons/trunk@20229 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.uka.ipd.sdq.probfunction/src-man/de/uka/ipd/sdq/probfunction/print/ProbFunctionPrettyPrint.java
+++ de.uka.ipd.sdq.probfunction/src-man/de/uka/ipd/sdq/probfunction/print/ProbFunctionPrettyPrint.java
@@ -2,7 +2,6 @@ package de.uka.ipd.sdq.probfunction.print;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.util.List;
 import java.util.Locale;
 
 import de.uka.ipd.sdq.probfunction.BoxedPDF;
@@ -17,6 +16,19 @@ import de.uka.ipd.sdq.probfunction.util.ProbfunctionSwitch;
 
 public class ProbFunctionPrettyPrint extends ProbfunctionSwitch<String> {
 
+    private static final int AVERAGE_PROBFUNCTION_LENGTH = 50;
+    private final StringBuilder resultBuilder;
+    private final static DecimalFormat df = new DecimalFormat("#0.0#######", new DecimalFormatSymbols(Locale.US));
+    
+    public ProbFunctionPrettyPrint() {
+        this(new StringBuilder(AVERAGE_PROBFUNCTION_LENGTH));
+    }
+    
+    public ProbFunctionPrettyPrint(final StringBuilder builder) {
+        super();
+        this.resultBuilder = builder;
+    }
+    
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -24,22 +36,20 @@ public class ProbFunctionPrettyPrint extends ProbfunctionSwitch<String> {
 	 */
 	@Override
 	public String caseBoxedPDF(BoxedPDF object) {
-		String sampleString = "";
-		for (ContinuousSample s : (List<ContinuousSample>) object.getSamples()) {
-			double value = s.getProbability();
-			//double precision = 0.00001;
-			double precision = 0.00000001;
+        resultBuilder.append("DoublePDF[");
+		for (ContinuousSample s : object.getSamples()) {
+			// double value = s.getProbability();
+			// double precision = 0.00001;
+			// Why needed as Decimal format rounds internally???
+			// double precision = 0.00000001;
 			
-			value *= 1 / precision;
-			long temp = Math.round(value);
-			double prob = temp * precision;
-			DecimalFormat df = new DecimalFormat("0.00000000", new DecimalFormatSymbols(Locale.US));
-			
-			sampleString += " (" + s.getValue() + "; " + df.format(prob)
-					+ ")";
+			// value *= 1 / precision;
+			// long temp = Math.round(value);
+			// double prob = temp * precision;
+			resultBuilder.append(String.format("(%s; %s)", df.format(s.getValue()), df.format(s.getProbability())));
 		}
-		
-		return "DoublePDF[" + sampleString + " ]";
+		resultBuilder.append("]");
+		return resultBuilder.toString();
 	}
 
 	/*
@@ -49,10 +59,9 @@ public class ProbFunctionPrettyPrint extends ProbfunctionSwitch<String> {
 	 */
 	@Override
 	public String caseProbabilityMassFunction(ProbabilityMassFunction object) {
-		Sample sample = (Sample)object.getSamples().get(0);
+		Sample<?> sample = object.getSamples().get(0);
 		
 		String pmfType = detectType(sample);
-		String sampleString = "";
 		String leftSeparator = "";
 		String rightSeparator = ";";
 
@@ -61,36 +70,54 @@ public class ProbFunctionPrettyPrint extends ProbfunctionSwitch<String> {
 			rightSeparator = "\";";
 		}
 		
-		for (Sample s : (List<Sample>) object.getSamples()) {
-			sampleString += " (" + leftSeparator + s.getValue() 
-			+ rightSeparator + s.getProbability() + ")";
+		resultBuilder.append(pmfType);
+		resultBuilder.append("[");
+		for (Sample<?> s : object.getSamples()) {
+		    resultBuilder.append(
+		            String.format("(%s%s %s%s)",
+		                    leftSeparator,
+		                    s.getValue(),
+		                    rightSeparator,
+		                    df.format(s.getProbability())));
 		}
-		return pmfType + "[" + sampleString + " ]";
+		resultBuilder.append("]");
+		return resultBuilder.toString();
 	}
 	
 	@Override
 	public String caseExponentialDistribution(ExponentialDistribution object) {
-		return "Exp("+object.getRate()+")";
+	    resultBuilder.append(String.format("Exp(%s)", 
+	            df.format(object.getRate())));
+		return resultBuilder.toString();
 	}
 	
 
 
 	@Override
 	public String caseGammaDistribution(GammaDistribution object) {
-		return "Gamma("+object.getAlpha()+", "+object.getTheta()+")";
+	    resultBuilder.append(String.format("Gamma(%s, %s)",
+	            df.format(object.getAlpha()),
+	            df.format(object.getTheta())));
+		return resultBuilder.toString();
 	}
 
 	@Override
 	public String caseLognormalDistribution(LognormalDistribution object) {
-		return "Lognorm("+object.getMu()+", "+object.getSigma()+")";
+	    resultBuilder.append(String.format("Lognorm(%s, %s)", 
+	            df.format(object.getMu()), 
+	            df.format(object.getSigma())));
+		return resultBuilder.toString();
 	}
 
 	@Override
 	public String caseNormalDistribution(NormalDistribution object) {
-		return "Norm("+object.getMu()+", "+object.getSigma()+")";
+	    resultBuilder.append(String.format("Norm(%s, %s)",
+	            df.format(object.getMu()),
+	            df.format(object.getSigma())));
+		return resultBuilder.toString();
 	}
 
-	private String detectType(Sample object) {
+	private String detectType(Sample<?> object) {
 		if (object.getValue() instanceof Integer)
 			return "IntPMF";
 		if (object.getValue() instanceof Double)
@@ -101,5 +128,4 @@ public class ProbFunctionPrettyPrint extends ProbfunctionSwitch<String> {
 			return "EnumPMF";
 		throw new UnsupportedOperationException("Unknown PMF found!");
 	}
-
 }

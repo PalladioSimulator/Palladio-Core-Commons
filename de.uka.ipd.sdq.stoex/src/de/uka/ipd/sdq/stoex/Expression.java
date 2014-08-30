b0a0e6a562479619f50bb322c31e9a6c28409465
jonasheinisch <jonas@heinisch-web.de>
2014-08-30 20:12:08 +0000

    added commons' branch for stoex migration
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Commons/branches/stoex-xtext-migration@24260 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.ipd.sdq.stoex/src/de/uka/ipd/sdq/stoex/Expression.java
@@ -0,0 +1,29 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.stoex;
+
+import edu.kit.ipd.sdq.mdsd.profiles.metamodelextension.EStereotypableObject;
+
+/**
+ * <!-- begin-user-doc --> A representation of the model object '<em><b>Expression</b></em>'. <!--
+ * end-user-doc -->
+ *
+ *
+ * @see de.uka.ipd.sdq.stoex.StoexPackage#getExpression()
+ * @model abstract="true"
+ * @extends EStereotypableObject
+ * @generated
+ */
+public interface Expression extends EStereotypableObject {
+
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    String copyright = "Copyright 2007-2009, SDQ, IPD, U Karlsruhe";
+} // Expression
---
+++ de.uka.ipd.sdq.stoex/src/de/uka/ipd/sdq/stoex/impl/ExpressionImpl.java
@@ -0,0 +1,50 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.stoex.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import de.uka.ipd.sdq.stoex.Expression;
+import de.uka.ipd.sdq.stoex.StoexPackage;
+import edu.kit.ipd.sdq.mdsd.profiles.metamodelextension.impl.EStereotypableObjectImpl;
+
+/**
+ * <!-- begin-user-doc --> An implementation of the model object '<em><b>Expression</b></em>'. <!--
+ * end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public abstract class ExpressionImpl extends EStereotypableObjectImpl implements Expression {
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    public static final String copyright = "Copyright 2007-2009, SDQ, IPD, U Karlsruhe";
+
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    protected ExpressionImpl() {
+        super();
+    }
+
+    /**
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    @Override
+    protected EClass eStaticClass() {
+        return StoexPackage.Literals.EXPRESSION;
+    }
+
+} // ExpressionImpl

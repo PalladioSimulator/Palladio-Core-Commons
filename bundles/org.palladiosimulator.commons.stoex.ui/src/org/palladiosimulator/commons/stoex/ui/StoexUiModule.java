9f10c1c867813370c827f649343b0c49b1fc280d
Stephan Seifermann <stephan.seifermann@kit.edu>
2021-02-16 17:14:46 +0100

    PALLADIO-530 Remove PCM specific aspects from Stoex.

--- bundles/org.palladiosimulator.commons.stoex.ui/src/org/palladiosimulator/commons/stoex/ui/StoexUiModule.java
+++ bundles/org.palladiosimulator.commons.stoex.ui/src/org/palladiosimulator/commons/stoex/ui/StoexUiModule.java
@@ -1,24 +1,11 @@
 package org.palladiosimulator.commons.stoex.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.eclipse.xtext.ui.editor.embedded.IEditedResourceProvider;
-import org.palladiosimulator.commons.stoex.ui.dialog.StoExEditDialogFactory;
-import org.palladiosimulator.commons.stoex.ui.dialog.impl.StoExEditDialogFactoryImpl;
-import org.palladiosimulator.commons.stoex.ui.dialog.impl.StoexEditedResourceProvider;
 
-@SuppressWarnings("restriction")
 public class StoexUiModule extends AbstractStoexUiModule {
 
     public StoexUiModule(final AbstractUIPlugin plugin) {
         super(plugin);
-    }
-
-    public Class<? extends StoExEditDialogFactory> bindStoExEditDialogFactory() {
-        return StoExEditDialogFactoryImpl.class;
-    }
-
-    public Class<? extends IEditedResourceProvider> bindIEditedResourceProvider() {
-        return StoexEditedResourceProvider.class;
     }
 
 }

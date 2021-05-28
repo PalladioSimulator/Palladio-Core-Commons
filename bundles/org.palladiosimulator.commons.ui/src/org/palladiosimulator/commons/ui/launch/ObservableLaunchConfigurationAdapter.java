3149291e61c935c9b6d55b75a50351b09aacdd0b
Sebastian Krach <krach@fzi.de>
2021-05-28 13:22:51 +0200

    Include code review remarks

--- bundles/org.palladiosimulator.commons.ui/src/org/palladiosimulator/commons/ui/launch/ObservableLaunchConfigurationAdapter.java
+++ bundles/org.palladiosimulator.commons.ui/src/org/palladiosimulator/commons/ui/launch/ObservableLaunchConfigurationAdapter.java
@@ -11,20 +11,21 @@ import org.eclipse.core.databinding.observable.value.WritableValue;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
 
 /**
  * This class serves as generic bridge between Eclipse Launch Configurations and the Eclipse databinding framework.
  * 
- * For detaiuls on its usage, please see {@link ObservableLaunchConfigurationAttributeFactory}.
+ * For details on its usage, please see {@link ObservableLaunchConfigurationAttributeFactory}.
  * 
  * @author Sebastian Krach
  *
  */
 public class ObservableLaunchConfigurationAdapter implements ObservableLaunchConfigurationAttributeFactory {
-    protected Map<String, Consumer<ILaunchConfigurationWorkingCopy>> lcUpdates = new HashMap<>();
-    protected Map<String, Consumer<ILaunchConfiguration>> mUpdates = new HashMap<>();
-    protected Map<String, Consumer<ILaunchConfigurationWorkingCopy>> defaults = new HashMap<>();
-    protected Map<String, IObservableValue<?>> observables = new HashMap<>();
+    protected final Map<String, Consumer<ILaunchConfigurationWorkingCopy>> lcUpdates = new HashMap<>();
+    protected final Map<String, Consumer<ILaunchConfiguration>> mUpdates = new HashMap<>();
+    protected final Map<String, Consumer<ILaunchConfigurationWorkingCopy>> defaults = new HashMap<>();
+    protected final Map<String, IObservableValue<?>> observables = new HashMap<>();
     private ISideEffect dirtyNotificationEffect;
     private Runnable dirtyNotification;
     private volatile boolean isInitialization = false;
@@ -33,6 +34,11 @@ public class ObservableLaunchConfigurationAdapter implements ObservableLaunchCon
         this.dirtyNotification = dirtyNotification;
     }
 
+    /**
+     * Initialize values from {@link ILaunchConfiguration}.
+     * 
+     * @see AbstractLaunchConfigurationTab#initializeFrom(ILaunchConfiguration)
+     */
     public void initializeFrom(final ILaunchConfiguration configuration) {
         if (dirtyNotificationEffect != null) {
             dirtyNotificationEffect.dispose();
@@ -50,11 +56,22 @@ public class ObservableLaunchConfigurationAdapter implements ObservableLaunchCon
         isInitialization = false;
     }
 
+    /**
+     * Apply changes to {@link ILaunchConfigurationWorkingCopy}
+     * 
+     * @see AbstractLaunchConfigurationTab#performApply(ILaunchConfigurationWorkingCopy)
+     * 
+     */
     public void performApply(ILaunchConfigurationWorkingCopy workingCopy) {
         lcUpdates.values()
             .forEach(c -> c.accept(workingCopy));
     }
 
+    /**
+     * Initialize launch configuration with defaults.
+     * 
+     * @see AbstractLaunchConfigurationTab#setDefaults(ILaunchConfigurationWorkingCopy)
+     */
     public void setDefaults(final ILaunchConfigurationWorkingCopy configuration) {
         defaults.values()
             .forEach(c -> c.accept(configuration));

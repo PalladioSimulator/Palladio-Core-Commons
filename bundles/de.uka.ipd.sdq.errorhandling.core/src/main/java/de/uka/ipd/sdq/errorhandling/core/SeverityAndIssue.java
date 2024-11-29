65ab8fca2addbe47b389b56f8d11ee8989938aa6
Ralf Sieger <sieger@fzi.de>
2024-11-29 08:34:28 +0100

    moved issue related classes to de.uka.ipd.sdq.errorhandling.core

--- bundles/de.uka.ipd.sdq.errorhandling.core/src/main/java/de/uka/ipd/sdq/errorhandling/core/SeverityAndIssue.java
+++ bundles/de.uka.ipd.sdq.errorhandling.core/src/main/java/de/uka/ipd/sdq/errorhandling/core/SeverityAndIssue.java
@@ -1,8 +1,6 @@
-package de.uka.ipd.sdq.errorhandling;
+package de.uka.ipd.sdq.errorhandling.core;
 
 import org.eclipse.emf.ecore.EObject;
-
-import de.uka.ipd.sdq.errorhandling.dialogs.issues.IssuesDialog;
 
 /**
  * Issue combined with a severity level. Used to record and report issues to the user using the
@@ -97,26 +95,30 @@ public class SeverityAndIssue implements Comparable<SeverityAndIssue> {
     public String getResourceName() {
         if (element != null) {
             if (element instanceof EObject) {
-                return ((EObject) element).eResource().getURI().toFileString();
+                return ((EObject) element).eResource()
+                    .getURI()
+                    .toFileString();
             } else {
                 return objectResourceName == null ? DEFAULT_RESOURCE_NAME : objectResourceName;
             }
-        } else
+        } else {
             return DEFAULT_RESOURCE_NAME;
-
+        }
     }
 
     public SeverityEnum getError() {
         return severity;
     }
 
+    @Override
     public int compareTo(SeverityAndIssue o) {
-        if (this.severity == o.getError())
+        if (this.severity == o.getError()) {
             return 0;
-        if (this.severity == SeverityEnum.ERROR && o.getError() == SeverityEnum.WARNING)
+        }
+        if (this.severity == SeverityEnum.ERROR && o.getError() == SeverityEnum.WARNING) {
             return -1;
-        else
-            return 1;
+        }
+        return 1;
     }
 
 }
